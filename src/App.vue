dce55635dabe262343206e7a4de0e7c2e3a72b54
sebastianmonten <sebastianmonten.tech@gmail.com>
2022-02-19 10:50:31 +0100

    Experiment with a hamburger button

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,14 @@
 <template>
+  <div class="top-icon" @click="showTopMenu = !showTopMenu">
+    <div class="main-item menu" v-if="!showTopMenu">
+      <span class="line line01"></span>
+      <span class="line line02"></span>
+      <span class="line line03"></span>
+    </div>
+    <div v-else>
+      X
+    </div>
+  </div>
   <h1>Sebastian Montén</h1>
 </template>
 
@@ -6,7 +16,12 @@
 
 export default {
   name: 'App',
-  components: {}
+  components: {},
+  data() {
+    return {
+      showTopMenu: true
+    }
+  }
 }
 </script>
 
@@ -22,5 +37,46 @@ export default {
 body {
   color: #ddd;
   background: #071b2b;
+}
+.top-icon {
+  background: #29afd1;
+  display: inline-block;
+  border-radius: 500px;
+  margin: 10px;
+  position: relative;
+  padding: 80px;
+  cursor: pointer;
+}
+
+.main-item {
+  width: 150px;
+  height: 150px;
+  position: relative;
+}
+
+.line {
+  position: absolute;
+  height: 15px;
+  width: 100%;
+  background: white;
+  border-radius: 10px;
+  transition: all cubic-bezier(0.25, 0.1, 0.28, 1.54) 0.32s;
+}
+
+.line01 {
+  top: 19%;
+}
+
+.line02 {
+  top: 49%;
+}
+
+.line03 {
+  top: 79%;
+}
+
+.menu.close .line01 {
+  transform: rotate(45deg);
+  top: 49%;
 }
 </style>
