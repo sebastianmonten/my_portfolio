48813fb14c332e129e8b76715ecdb2ad08f14d0c
sebastianmonten <94540990+sebastianmonten@users.noreply.github>
2024-02-03 09:03:46 +0100

    feat: add my profile logo as a component and update color of name above it

---
+++ src/components/MyLogo.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="my-profile-container">
+    <!-- Image with round corners -->
+    <img src="../assets/profile.png" alt="Profile logo" class="rounded-image">
+  </div>
+</template>
+
+
+<style>
+/* CSS to make the image round */
+.my-profile-container {
+  width: 160px; /* Adjust this value to your desired square size */
+  height: auto; /* Same as width to make it square */
+  padding: 10px;
+}
+.rounded-image {
+  border-radius: 50%; /* Makes the image circular */
+  width: 100%
+}
+
+</style>
